fd79bd30e736c52b0645718ee2531adf3b8dab81
bspatafora <benjamin.spatafora@gmail.com>
2014-05-08 16:19:22 -0500

    Add horizonal_victory?, vertical_victory?, and diagonal_victory? methods and specs to Game

--- lib/tic_tac_toe/game.rb
+++ lib/tic_tac_toe/game.rb
@@ -4,6 +4,7 @@ module TicTacToe
 
     def initialize
       @board = Array.new(9)
+      @row_size = Math.sqrt(@board.size)
       @tokens = [:X, :O]
     end
 
@@ -14,6 +15,32 @@ module TicTacToe
       else
         raise InvalidMove
       end
+    end
+
+    def diagonal_victory?(token)
+      back_diagonal, front_diagonal = true, true
+      generate_row_slices.each_with_index do |row, index|
+        back_diagonal = false if row[index] != token
+        front_diagonal = false if row[@row_size - (index + 1)] != token
+      end
+      back_diagonal || front_diagonal
+    end
+
+    def horizontal_victory?(token)
+      linear_victory?(generate_row_slices, token)
+    end
+
+    def vertical_victory?(token)
+      transposed_rows = generate_row_slices.transpose
+      linear_victory?(transposed_rows, token)
+    end
+
+    def generate_row_slices
+      @board.each_slice(@row_size).to_a
+    end
+
+    def linear_victory?(rows, token)
+      rows.any? { |row| row.all? { |space| space == token  } }
     end
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe TicTacToe::Game do
-  describe '#place' do
-    let(:game) { TicTacToe::Game.new }
+  let(:game) { TicTacToe::Game.new }
 
+  describe '#place' do
     it "places a valid move" do
       game.place(0)
       expect(game.board[0]).to eql(:X)
@@ -22,6 +22,55 @@ describe TicTacToe::Game do
 
     it "doesn't place a move if it's off the board" do
       expect{ game.place(9) }.to raise_error(TicTacToe::InvalidMove)
+    end
+  end
+
+  describe '#diagonal_victory?' do
+    it "returns false if a token has not achieved diagonal victory" do
+      expect(game.diagonal_victory?(:X)).to be false
+    end
+
+    it "returns true if a token has achieved back diagonal victory" do
+      string = "07468"
+      generate_board_from(string, game)
+      expect(game.diagonal_victory?(:X)).to be true
+    end
+
+    it "returns true if a token has achieved front diagonal victory" do
+      string = "20416"
+      generate_board_from(string, game)
+      expect(game.diagonal_victory?(:X)).to be true
+    end
+  end
+
+  describe '#horizonal_victory?' do
+    it "returns false if a token has not achieved horizonatal victory" do
+      expect(game.horizontal_victory?(:X)).to be false
+    end
+
+    it "returns true if a token has achieved horizonatal victory" do
+      string = "08172"
+      generate_board_from(string, game)
+      expect(game.horizontal_victory?(:X)).to be true
+    end
+  end
+
+  describe '#vertical_victory?' do
+    it "returns false if a token has not achieved vertical victory" do
+      expect(game.vertical_victory?(:X)).to be false
+    end
+
+    it "returns true if a token has achieved horizonatal victory" do
+      string = "08376"
+      generate_board_from(string, game)
+      expect(game.vertical_victory?(:X)).to be true
+    end
+  end
+
+  def generate_board_from(string, game)
+    string.each_char do |char|
+      move = char.to_i
+      game.place(move)
     end
   end
 end
