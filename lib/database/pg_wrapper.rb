93ba0d24bb5d7d941854074209feabca11271615
bspatafora <benjamin.spatafora@gmail.com>
2014-06-12 09:20:36 -0500

    Refactor PGWrapper

--- lib/database/pg_wrapper.rb
+++ lib/database/pg_wrapper.rb
@@ -7,26 +7,17 @@ module Database
     end
 
     def record_game(history)
-      connection = PG.connect(dbname: @database)
+      connection = establish_connection
       create_tables_if_needed(connection)
-
-      connection.exec("INSERT INTO games (board_size, winner) VALUES (
-        #{history.board_size},
-        '#{history.winner}')")
-
-      game_id_result = connection.exec("SELECT currval(pg_get_serial_sequence('games','id'))")
-
-      history.moves.each_with_index do |move, index|
-        move_number = index + 1
-        connection.exec("INSERT INTO moves (game, number, token, space) VALUES (
-          #{game_id_result.getvalue(0,0)},
-          #{move_number},
-          '#{move.first}',
-          #{move.last})")
-      end
+      record_board_size_and_winner(history, connection)
+      record_moves(history, connection)
     end
 
     private
+
+    def establish_connection
+      PG.connect(dbname: @database)
+    end
 
     def create_tables_if_needed(connection)
       connection.exec("CREATE TABLE IF NOT EXISTS games (
@@ -38,6 +29,30 @@ module Database
         number integer,
         token char(1),
         space integer)")
+    end
+
+    def record_board_size_and_winner(history, connection)
+      connection.exec("INSERT INTO games (board_size, winner) VALUES (
+        #{history.board_size},
+        '#{history.winner}')")
+    end
+
+    def record_moves(history, connection)
+      game_id = get_game_id(connection)
+
+      history.moves.each_with_index do |move, index|
+        move_number = index + 1
+        connection.exec("INSERT INTO moves (game, number, token, space) VALUES (
+          #{game_id},
+          #{move_number},
+          '#{move.first}',
+          #{move.last})")
+      end
+    end
+
+    def get_game_id(connection)
+      result = connection.exec("SELECT currval(pg_get_serial_sequence('games','id'))")
+      result.getvalue(0,0)
     end
   end
 end
