95da75d63f7ca94f6312ae07d6943951bbe14a90
bspatafora <benjamin.spatafora@gmail.com>
2014-06-13 13:33:35 -0500

    Add read game histories functionality to PGWrapper

--- lib/database/pg_wrapper.rb
+++ lib/database/pg_wrapper.rb
@@ -1,4 +1,5 @@
 require 'pg'
+require 'tic_tac_toe/history'
 
 module Database
   class PGWrapper
@@ -6,17 +7,48 @@ module Database
       @database = database
     end
 
-    def record_game(history)
+    def record_game_history(history)
       connection = establish_connection
       create_tables_if_needed(connection)
       record_board_size_and_winner(history, connection)
       record_moves(history, connection)
     end
 
+    def read_game_histories
+      connection = establish_connection
+      games_result = connection.exec("SELECT * FROM games")
+      games = []
+
+      games_result.each_row do |row|
+        game_id = row[0]
+        board_size = row[1].to_i
+        winner = row[2]
+        moves_result = connection.exec("SELECT * FROM moves WHERE game = #{game_id}")
+
+        history = TicTacToe::History.new
+
+        moves_result.each_row do |row|
+          token, space = row[2], row[3].to_i
+          history.record_move([token, space])
+        end
+        history.record_board_size(board_size)
+        history.record_winner(winner)
+
+        games << history
+      end
+
+      games
+    end
+
     private
 
     def establish_connection
-      PG.connect(dbname: @database)
+      connection = PG.connect(dbname: @database)
+
+      # Suppress PostgreSQL notices that tables already exist
+      connection.exec("set client_min_messages=warning")
+
+      connection
     end
 
     def create_tables_if_needed(connection)
@@ -38,7 +70,7 @@ module Database
     end
 
     def record_moves(history, connection)
-      game_id = get_game_id(connection)
+      game_id = read_game_id(connection)
 
       history.moves.each_with_index do |move, index|
         move_number = index + 1
@@ -50,7 +82,7 @@ module Database
       end
     end
 
-    def get_game_id(connection)
+    def read_game_id(connection)
       result = connection.exec("SELECT currval(pg_get_serial_sequence('games','id'))")
       result.getvalue(0,0)
     end
--- spec/database/pg_wrapper_spec.rb
+++ spec/database/pg_wrapper_spec.rb
@@ -1,4 +1,5 @@
 require 'database/pg_wrapper'
+require 'pg'
 require 'tic_tac_toe/history'
 require 'tic_tac_toe/spec_helper'
 
@@ -6,15 +7,42 @@ describe Database::PGWrapper do
   database = "test"
   let(:pg_wrapper) { Database::PGWrapper.new(database) }
 
-  describe '#record_game' do
-    it "sends a history object using the pg gem" do
-      history = TicTacToe::History.new
-      history.record_board_size(9)
-      history.record_move(["X", 1])
-      history.record_move(["O", 4])
-      history.record_winner("X")
+  before do
+    @history1 = TicTacToe::History.new
+    @history1.record_board_size(9)
+    @history1.record_move(["X", 1])
+    @history1.record_move(["O", 4])
+    @history1.record_winner("X")
 
-      expect { pg_wrapper.record_game(history) }.not_to raise_error
+    @history2 = TicTacToe::History.new
+    @history2.record_board_size(16)
+    @history2.record_move(["&", 14])
+    @history2.record_winner("*")
+  end
+
+  describe '#record_game_history and #read_games' do
+    it "records and reads history objects to and from the database" do
+      pg_wrapper.record_game_history(@history1)
+      pg_wrapper.record_game_history(@history2)
+
+      games = pg_wrapper.read_game_histories
+      history1 = games[0]
+      history2 = games[1]
+
+      expect(history1.board_size).to eq(9)
+      expect(history1.moves[0]).to eq(["X", 1])
+      expect(history1.moves[1]).to eq(["O", 4])
+      expect(history1.winner).to eq("X")
+
+      expect(history2.board_size).to eq(16)
+      expect(history2.moves[0]).to eq(["&", 14])
+      expect(history2.winner).to eq("*")
     end
+  end
+
+  after do
+    connection = PG.connect(dbname: database)
+    connection.exec("DROP TABLE moves")
+    connection.exec("DROP TABLE games")
   end
 end
