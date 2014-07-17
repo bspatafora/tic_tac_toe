45cb27c3e485bb2c69d2d6e0876d2498ff31b53b
bspatafora <benjamin.spatafora@gmail.com>
2014-07-17 14:48:40 -0500

    Add UI::Adapter

---
+++ lib/ui/adapter.rb
@@ -0,0 +1,70 @@
+require 'tic_tac_toes/board'
+require 'tic_tac_toes/game_state'
+require 'tic_tac_toes/player_factory'
+
+module UI
+  class NullHistory
+    def record_board_size(size)
+      nil
+    end
+
+    def record_move(move)
+      nil
+    end
+  end
+
+  module Adapter
+    def self.new_board_structure
+      board = TicTacToes::Board.new
+      board.spaces
+    end
+
+    def self.game_state_from_board_structure(board_structure)
+      player_factory = TicTacToes::PlayerFactory.new('unused_io')
+      human_player = player_factory.generate_human_player('X')
+      computer_player = player_factory.generate_computer_player('O', :hard)
+      players = [human_player, computer_player]
+
+      board_structure_with_players = replace_tokens_with_players(board_structure, human_player, computer_player)
+      board = board_from_structure(board_structure_with_players)
+
+      TicTacToes::GameState.new(board, players, NullHistory.new)
+    end
+
+    def self.game_state_to_board_structure(game_state)
+      structure_with_players = game_state.board.spaces
+
+      replace_players_with_tokens(structure_with_players)
+    end
+
+    private
+
+    def self.replace_tokens_with_players(board_structure, human_player, computer_player)
+      board_structure.map do |space|
+        case space
+        when 'X'
+          human_player
+        when 'O'
+          computer_player
+        else
+          nil
+        end
+      end
+    end
+
+    def self.replace_players_with_tokens(board_structure)
+      board_structure.map { |space| space.token unless space.nil? }
+    end
+
+    def self.board_from_structure(board_structure)
+      row_size = Math.sqrt(board_structure.count).to_i
+      board = TicTacToes::Board.new(row_size: row_size)
+
+      board_structure.each_with_index do |player, index|
+        board.place(player, index)
+      end
+
+      board
+    end
+  end
+end
---
+++ spec/ui/adapter_spec.rb
@@ -0,0 +1,43 @@
+require 'tic_tac_toes/board'
+require 'tic_tac_toes/game_state'
+require 'tic_tac_toes/player_factory'
+require 'ui/adapter'
+
+describe UI::Adapter do
+  describe '#new_board_structure' do
+    it 'returns a new board structure' do
+      new_board_structure = [nil, nil, nil, nil, nil, nil, nil, nil, nil]
+      expect(UI::Adapter.new_board_structure).to eq(new_board_structure)
+    end
+  end
+
+  describe '#game_state_from_board_structure' do
+    it 'returns a game state object based on a board structure' do
+      board_structure = [nil, nil, nil, nil, "X", nil, nil, nil, nil]
+
+      game_state = UI::Adapter.game_state_from_board_structure(board_structure)
+      middle_space = game_state.board.space(4)
+      first_player = game_state.players.first
+      second_player = game_state.players.last
+
+      expect(middle_space.token).to eq('X')
+      expect(first_player.token).to eq('X')
+      expect(second_player.token).to eq('O')
+    end
+  end
+
+  describe '#game_state_to_board_structure' do
+    it 'returns a board structure based on a game state object' do
+      player_factory = TicTacToes::PlayerFactory.new('unused_io')
+      first_player = player_factory.generate_human_player('X')
+      second_player = player_factory.generate_computer_player('O', :hard)
+      players = [first_player, second_player]
+      board = TicTacToes::Board.new
+      board.place(first_player, 4)
+
+      game_state = TicTacToes::GameState.new(board, players, UI::NullHistory.new)
+      board_structure = [nil, nil, nil, nil, "X", nil, nil, nil, nil]
+      expect(UI::Adapter.game_state_to_board_structure(game_state)).to eq(board_structure)
+    end
+  end
+end
